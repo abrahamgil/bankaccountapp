e3a1d3162a1a0703a7b1df977cb30298dad1e326
Abraham <abrahamgil2@gmail.com>
2020-03-29 08:01:59 -0500

    created deposit, withdraw, and commit methods and are now able to deposit, withdraw, and update our balance

--- acc.py
+++ acc.py
@@ -1,8 +1,24 @@
-class Account:
+#classes used in OOP
 
+class Account:
+    #method
     def __init__(self, filepath):
+        self.tempfilepath=filepath #instance variable to be used everywhere in class
         with open(filepath,'r') as file:
-            self.balance=int(file.read())
+            self.balance=int(file.read()) #attribute
+
+    def withdraw(self, amount):
+        self.balance=self.balance - amount
+
+    def deposit(self, amount):
+        self.balance=self.balance + amount
+    
+    def commit(self):
+        with open(self.tempfilepath, 'w') as file:
+            file.write(str(self.balance))
 
 account=Account("balance.txt")
 print(account.balance)
+account.withdraw(100)
+print(account.balance)
+account.commit()
