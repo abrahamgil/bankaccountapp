c65381c85be552bd91580b468e4178024af0d2c1
Abraham <abrahamgil2@gmail.com>
2020-03-30 00:58:46 -0500

    Created multiple bank accounts and worked on playing with different objects and understanding class structures

--- acc.py
+++ acc.py
@@ -2,10 +2,10 @@
 
 class Account:
     #method
-    def __init__(self, filepath):
-        self.tempfilepath=filepath #instance variable to be used everywhere in class
+    def __init__(self, filepath): #constructor
+        self.tempfilepath=filepath #object instance  to be used everywhere in class
         with open(filepath,'r') as file:
-            self.balance=int(file.read()) #attribute
+            self.balance=int(file.read())
 
     def withdraw(self, amount):
         self.balance=self.balance - amount
@@ -17,18 +17,28 @@ class Account:
         with open(self.tempfilepath, 'w') as file:
             file.write(str(self.balance))
 
-#inherits the Account class
+
 class Checking(Account):
+    """This class generates checking account objects"""
+    type="checking" #class variable
 
     def __init__(self, filepath, fee):
         Account.__init__(self, filepath)
         self.fee=fee
 
-    def transfer(self, amount):
+    def transfer(self, amount): #method
         self.balance=self.balance - amount - self.fee
 
-checking=Checking("balance.txt", 1)
-checking.transfer(110)
-print(checking.balance)
-checking.commit()
-    
+jack_checking=Checking("jack.txt", 1)
+jack_checking.transfer(100)
+print(jack_checking.balance)
+jack_checking.commit()
+print(jack_checking.type)
+
+john_checking=Checking("john.txt", 1)
+john_checking.transfer(100)
+print(john_checking.balance)
+john_checking.commit()
+print(john_checking.type)
+
+print(john_checking.__doc__)
